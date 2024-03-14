688959de969359de391b1f0332dc46665f49f74c
Allanio <allanio@hotmail.co.uk>
2024-03-14 23:06:30 +0000

    feat: card actions
    delete board

--- components/actions.tsx
+++ components/actions.tsx
@@ -8,8 +8,10 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
-import { Link2 } from "lucide-react";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
 
 interface ActionsProps {
     children: React.ReactNode
@@ -25,12 +27,19 @@ export const Actions = ({
                             id,
                             title
                         }: ActionsProps) => {
+    const {mutate, loading} = useApiMutation(api.board.remove)
     const onCopyLink = () => {
         navigator.clipboard.writeText(
             `${window.location.origin}/board/${id}`
         )
             .then(()=> toast.success("Link copied to clipboard"))
             .catch(()=> toast.error("Failed to copy link"))
+    }
+
+    const onDelete = () => {
+        mutate({id})
+            .then(() => toast.success('Board deleted successfully'))
+            .catch(() => toast.error("Failed to delete board"))
     }
 
     return (
@@ -51,6 +60,13 @@ export const Actions = ({
                     >
                         <Link2 className="h-4 w-4 mr-2"/>
                         Copy board link
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        className="p-3 cursor-pointer"
+                        onClick={onDelete}
+                    >
+                        <Trash2 className="h-4 w-4 mr-2"/>
+                        Delete
                     </DropdownMenuItem>
 
                 </DropdownMenuContent>
